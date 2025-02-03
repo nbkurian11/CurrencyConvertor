0213e9a043e20b5367fa9f5564347dfe13f681ac
Noel Kurian <70000001+nbkurian11@users.noreply.github.com>
2025-02-03 14:54:44 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+package cc;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Here are the different currencies you can convert:\n"
+				+ "1. USD \n2. CAD \n3. EUR");
+		
+
+	}
+
+}
