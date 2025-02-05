2bf7a03f6dd724911d2146c37beeb6f9dc58d414
Noel Kurian <70000001+nbkurian11@users.noreply.github.com>
2025-02-04 23:49:52 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,12 +1,20 @@
 package cc;
+import java.util.Scanner;
 
 public class Main {
+	
+	static Scanner x;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		System.out.println("Here are the different currencies you can convert:\n"
 				+ "1. USD \n2. CAD \n3. EUR");
+		
+		x = new Scanner(System.in);
+		int y = x.nextInt();
+		
+		//use exchange rate api
 		
 
 	}
